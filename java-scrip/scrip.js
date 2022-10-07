1d0e826eb0eed3ba3a5bd9b90f7745ab3a27fc86
lorena252 <lorenaavalos252@hotmail.com>
2022-10-07 03:46:40 -0300

    elimine los commits

--- java-scrip/scrip.js
+++ java-scrip/scrip.js
@@ -1,28 +1,22 @@
 let password = document.getElementById("password")
 const btnCopia =  document.getElementById("btn-copia")
 const btnGenerar =  document.getElementById("btn-generar")
-
 const inputDoce = document.getElementById("input-doce")
 const inputNueve = document.getElementById("input-nueve")
 const inputSeis = document.getElementById("input-seis")
 const inputLetras = document.getElementById("input-letras")
 const inputSoloNumeros = document.getElementById("input-solo-numeros")
 const inputCaracteres = document.getElementById("input-caracteres")
-
 const inputMayusculas = document.getElementById("check-mayuscula")
 const inputMinusculas = document.getElementById("check-minuscula")
 const inputNumeros = document.getElementById("check-numeros") 
 const inputSimbolos = document.getElementById("check-simbolos") 
-
-
 const opcionSeleccionada = []   
 const mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const minusculas = "abcdefghijklmnopkrstuvwxyz"
 const numeros =  "123456789"
 const simbolos = "%$^&!#"
 
-
-//eventos de cheked de reglas//
 inputLetras.addEventListener ("change", (e) =>{
   inputNumeros.disabled = !event.target.cheked;
   inputSimbolos.disabled = !event.target.cheked;
@@ -30,14 +24,12 @@ inputLetras.addEventListener ("change", (e) =>{
   inputMinusculas.disabled = event.target.cheked;
 }, false);
 
-
 inputSoloNumeros.addEventListener("change", (e) =>{
 inputMayusculas.disabled = !event.target.cheked;
 inputMinusculas.disabled = !event.target.cheked;
 inputSimbolos.disabled = !event.target.cheked;
 inputNumeros.disabled = event.target.cheked;
  }, false);
-
 
  inputCaracteres.addEventListener("change", (e) =>{
   if (inputCaracteres.checked)
@@ -47,9 +39,7 @@ inputSimbolos.disabled = event.target.cheked;
 inputNumeros.disabled = event.target.cheked;
 }, false)
 
-
-//inputs caja caracteres que estan chekeados//
-  inputMayusculas.addEventListener("click",  ()=> {
+inputMayusculas.addEventListener("click",  ()=> {
     if (inputMayusculas.checked) {
        opcionSeleccionada.push(mayusculas) 
     } else{
@@ -73,7 +63,6 @@ inputNumeros.addEventListener("click", () => {
     opcionSeleccionada.pop(numeros)
   }
 })
- 
 
 inputSimbolos.addEventListener("click", () =>{
 if (inputSimbolos.checked){
@@ -85,52 +74,38 @@ if (inputSimbolos.checked){
 })
 
 
-//concatena mi seleccion de opciones//
-
-// let contraseña = ""
-
- const concatenarOpciones = () =>{
+const concatenarOpciones = () =>{
    contraseña= (opcionSeleccionada.join(''))
-  // return contraseña
 } 
 
- //PINTA EL INPUT PASSWORD / Se me concatenan los datos//
 
 btnGenerar.addEventListener("click" , () =>{
-
  concatenarOpciones()
  generarContraseniaFinal(longitud)
      document.getElementById("password").value = clave;
-clave = ""
-
+ clave = ""
 })
 
-
 btnCopia.addEventListener("click", (e)=>{
-event.preventDefault();
-password.select();
-document.execCommand("copy");
-password.style.backgroundColor = "yellow" 
-
+ event.preventDefault();
+ password.select();
+ document.execCommand("copy");
+ password.style.backgroundColor = "yellow" 
 } )
 
 
-//Probando Longitud//
-
-
 let longitud = []
-//longitud doce caracteres//
 inputDoce.addEventListener("change", ()=>{
-if (inputDoce.checked){
+ if (inputDoce.checked){
   let numero = 12
-longitud.push(numero)
-}
-else{
+ longitud.push(numero)
+ }
+ else{
   longitud = []
 }
 })
 
-//longitud nueve caracteres//
+
 inputNueve.addEventListener("change", ()=>{
   if (inputNueve.checked){
     let numero = 9
@@ -141,7 +116,7 @@ inputNueve.addEventListener("change", ()=>{
   }
   })
 
-  //longitud seis caracteres// 
+
   inputSeis.addEventListener("change", ()=>{
     if (inputSeis.checked){
       let numero = 6
@@ -152,23 +127,12 @@ inputNueve.addEventListener("change", ()=>{
     }
     })
 
-// Me genera la clave!!! // 
 
 let clave = ""
-
 const generarContraseniaFinal = (longitud) =>{
- 
   for (let x = 0; x < longitud; x++){
 let aleatorio =  Math.floor(Math.random() * contraseña.length);
 clave+= contraseña.charAt(aleatorio);
   } 
-
 return clave
-
 };
-
-
-
-
-
-
