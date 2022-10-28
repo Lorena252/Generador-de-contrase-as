90f772336d08178d499333fcfe4c9284f5474d99
lorena252 <lorenaavalos252@hotmail.com>
2022-10-28 16:28:16 -0300

    acomode el codigo de js

--- js/scrip.js
+++ js/scrip.js
@@ -1,152 +1,152 @@
-let password = document.getElementById("password")
-const btnCopia =  document.getElementById("btn-copia")
-const btnGenerar =  document.getElementById("btn-generar")
-const inputDoce = document.getElementById("input-doce")
-const inputNueve = document.getElementById("input-nueve")
-const inputSeis = document.getElementById("input-seis")
-const inputLetras = document.getElementById("input-letras")
-const inputSoloNumeros = document.getElementById("input-solo-numeros")
-const inputCaracteres = document.getElementById("input-caracteres")
-const inputMayusculas = document.getElementById("check-mayuscula")
-const inputMinusculas = document.getElementById("check-minuscula")
-const inputNumeros = document.getElementById("check-numeros") 
-const inputSimbolos = document.getElementById("check-simbolos") 
-const opcionSeleccionada = []   
-const mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const minusculas = "abcdefghijklmnopkrstuvwxyz"
-const numeros =  "123456789"
-const simbolos = "%$^&!#"
+let password = document.getElementById("password");
+const btnCopia = document.getElementById("btn-copia");
+const btnGenerar = document.getElementById("btn-generar");
+const inputDoce = document.getElementById("input-doce");
+const inputNueve = document.getElementById("input-nueve");
+const inputSeis = document.getElementById("input-seis");
+const inputLetras = document.getElementById("input-letras");
+const inputSoloNumeros = document.getElementById("input-solo-numeros");
+const inputCaracteres = document.getElementById("input-caracteres");
+const inputMayusculas = document.getElementById("check-mayuscula");
+const inputMinusculas = document.getElementById("check-minuscula");
+const inputNumeros = document.getElementById("check-numeros");
+const inputSimbolos = document.getElementById("check-simbolos");
+const opcionSeleccionada = [];
+const mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const minusculas = "abcdefghijklmnopkrstuvwxyz";
+const numeros = "123456789";
+const simbolos = "%$^&!#";
 
+let longitud = [];
 
-
-let longitud = []
-
-inputDoce.addEventListener("change", ()=>{
- if (inputDoce.checked){
-    let numero = 12
-    longitud.push(numero)
- }
- else{
-  longitud.pop()
-}})
-
-
-inputNueve.addEventListener("change", ()=>{
-  if (inputNueve.checked){
-    let numero = 9
-    longitud.push(numero)
+inputDoce.addEventListener("change", () => {
+  if (inputDoce.checked) {
+    let numero = 12;
+    longitud.push(numero);
+  } else {
+    longitud.pop();
   }
-  else{
-    longitud.pop()
-  }})
-  
+});
 
-  inputSeis.addEventListener("change", ()=>{
-    if (inputSeis.checked){
-      let numero = 6
-      longitud.push(numero)
-    }
-    else{
-      longitud.pop()
-    }})
-    
-inputLetras.addEventListener ("change", (e) =>{
-  opcionSeleccionada.length = 0;
-  inputSimbolos.checked = false
-  inputNumeros.checked = false
-  if(inputLetras.checked)
-  inputNumeros.disabled = event.target.checked 
-  inputSimbolos.disabled = event.target.checked;
-  inputMayusculas.disabled = !event.target.checked;
-  inputMinusculas.disabled = !event.target.checked;
-}, false);
+inputNueve.addEventListener("change", () => {
+  if (inputNueve.checked) {
+    let numero = 9;
+    longitud.push(numero);
+  } else {
+    longitud.pop();
+  }
+});
 
-inputSoloNumeros.addEventListener("change", (e) =>{
-  opcionSeleccionada.length = 0;
-  inputMayusculas.checked = false
-  inputMinusculas.checked = false
-  inputSimbolos.checked = false
-  if(inputSoloNumeros.checked) 
-  inputMayusculas.disabled= event.target.checked;
-  inputMinusculas.disabled = event.target.checked;
-  inputSimbolos.disabled = event.target.checked;
-  inputNumeros.disabled = !event.target.checked 
- },false)
+inputSeis.addEventListener("change", () => {
+  if (inputSeis.checked) {
+    let numero = 6;
+    longitud.push(numero);
+  } else {
+    longitud.pop();
+  }
+});
 
- inputCaracteres.addEventListener("change", (e) =>{
-  if(inputCaracteres.checked)
-inputMayusculas.disabled = !event.target.checked;
-inputMinusculas.disabled = !event.target.checked;
-inputSimbolos.disabled = !event.target.checked;
-inputNumeros.disabled = !event.target.checked;
-},false)
+inputLetras.addEventListener(
+  "change",
+  (e) => {
+    opcionSeleccionada.length = 0;
+    inputSimbolos.checked = false;
+    inputNumeros.checked = false;
+    if (inputLetras.checked) inputNumeros.disabled = event.target.checked;
+    inputSimbolos.disabled = event.target.checked;
+    inputMayusculas.disabled = !event.target.checked;
+    inputMinusculas.disabled = !event.target.checked;
+  },
+  false
+);
 
-inputMayusculas.addEventListener("click",  ()=> {
-    if(inputMayusculas.checked) {
-       opcionSeleccionada.push(mayusculas) 
-    }else{
-      opcionSeleccionada.pop()
-    }})
- 
+inputSoloNumeros.addEventListener(
+  "change",
+  (e) => {
+    opcionSeleccionada.length = 0;
+    inputMayusculas.checked = false;
+    inputMinusculas.checked = false;
+    inputSimbolos.checked = false;
+    if (inputSoloNumeros.checked)
+      inputMayusculas.disabled = event.target.checked;
+    inputMinusculas.disabled = event.target.checked;
+    inputSimbolos.disabled = event.target.checked;
+    inputNumeros.disabled = !event.target.checked;
+  },
+  false
+);
 
-inputMinusculas.addEventListener("click", () =>{
-if (inputMinusculas.checked){
-  opcionSeleccionada.push(minusculas)
-}else{
-  opcionSeleccionada.pop()
-}
-})
+inputCaracteres.addEventListener(
+  "change",
+  (e) => {
+    if (inputCaracteres.checked)
+      inputMayusculas.disabled = !event.target.checked;
+    inputMinusculas.disabled = !event.target.checked;
+    inputSimbolos.disabled = !event.target.checked;
+    inputNumeros.disabled = !event.target.checked;
+  },
+  false
+);
+
+inputMayusculas.addEventListener("click", () => {
+  if (inputMayusculas.checked) {
+    opcionSeleccionada.push(mayusculas);
+  } else {
+    opcionSeleccionada.pop();
+  }
+});
+
+inputMinusculas.addEventListener("click", () => {
+  if (inputMinusculas.checked) {
+    opcionSeleccionada.push(minusculas);
+  } else {
+    opcionSeleccionada.pop();
+  }
+});
 
 inputNumeros.addEventListener("click", () => {
-  if (inputNumeros.checked){
-     opcionSeleccionada.push(numeros)  
+  if (inputNumeros.checked) {
+    opcionSeleccionada.push(numeros);
+  } else {
+    opcionSeleccionada.pop();
   }
-  else{
- opcionSeleccionada.pop()
+});
+
+inputSimbolos.addEventListener("click", () => {
+  if (inputSimbolos.checked) {
+    opcionSeleccionada.push(simbolos);
+  } else {
+    opcionSeleccionada.pop();
   }
-})
+});
 
-inputSimbolos.addEventListener("click", () =>{
-  if(inputSimbolos.checked){
-   opcionSeleccionada.push(simbolos) 
-}
-else{
-    opcionSeleccionada.pop()
-   }
-})
- 
-
-const concatenarOpciones = () =>{
-   contraseña= (opcionSeleccionada.join(''))
-} 
-
-
-let clave = ""
-const generarContraseniaFinal = (longitud) =>{
-  longitud = longitud[longitud.length-1]
-  for (let x = 0; x < longitud; x++){
-let aleatorio =  Math.floor(Math.random() * contraseña.length);
-clave+= contraseña.charAt(aleatorio);
-  }
-return clave
-
+let contraseña = "";
+const concatenarOpciones = () => {
+  contraseña = opcionSeleccionada.join("");
+  return contraseña;
 };
 
+let clave = "";
+const generarContraseniaFinal = (longitud) => {
+  longitud = longitud[longitud.length - 1];
+  for (let x = 0; x < longitud; x++) {
+    let aleatorio = Math.floor(Math.random() * contraseña.length);
+    clave += contraseña.charAt(aleatorio);
+  }
+  return clave;
+};
 
-btnGenerar.addEventListener("click" , () =>{
-  opcionSeleccionada
-  concatenarOpciones()
-  generarContraseniaFinal(longitud)
-      document.getElementById("password").value = clave;
-  clave = ""
- })
- 
- btnCopia.addEventListener("click", (e)=>{
- event.preventDefault();
-   password.select();
+btnGenerar.addEventListener("click", () => {
+  opcionSeleccionada;
+  concatenarOpciones();
+  generarContraseniaFinal(longitud);
+  document.getElementById("password").value = clave;
+  clave = "";
+});
+
+btnCopia.addEventListener("click", (e) => {
+  event.preventDefault();
+  password.select();
   document.execCommand("copy");
- password.style.backgroundColor = "yellow" 
- })
- 
-
-
+  password.style.backgroundColor = "yellow";
+});
